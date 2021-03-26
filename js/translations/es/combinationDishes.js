dd8dc881ddbc8b119a6de1e14e591ba67cc3dc1d
yipeiweb <peiyiliu88@gmail.com>
2021-03-26 11:59:22 +0100

    Add Combination dishes images

--- js/translations/es/combinationDishes.js
+++ js/translations/es/combinationDishes.js
@@ -1,29 +1,101 @@
 export default {
     title: 'Platos combinados',
     content: {
-        'Pechuga de pollo rebozado con patata': 5.5,
-        'Croquetas con patatas fritas y huevos': 4,
-        'Calamares a la romana con patata': 6.8,
-        'Lomo con huevo y patatas': 5,
-        'Salchicas con huevo y patatas fritas': 4.5,
-        'Frankfurt con patata': 4,
-        'Butifarra con judía, huevo y pimiento verde': 5.5,
-        'Bistec con patatas': 6.8,
-        'Espárrago a la plancha con jamón y tortilla a la francesa': 5.5,/**/
-        'Beicon, chistorra, huevo y patatas': 5.5,
-        'Chuletas de cerdo a la plancha': 5.5,
-        'Chuletas de corderos a la plancha': 6.5,
-        'Merluza rebozada con patatas': 5.5,
-        'Merluza a la plancha con patatas': 5.5,
-        'Lenguado a la plancha con patatas': 6.8,
-        'Sepia a la plancha con patatas': 8,
-        'Hamburguesa con huevos, ensalada y patatas': 5.8,
-        'Chistorra, jamon, huevos y patatas': 6.5,
-        'Combinados fritos': 6.8,
-        'Hamburguesa con patatas': 5,
-        'Lenguado frito con patatas': 6.8,
-        'Pincho de cerdo con patatas': 4.8,
-        'Pechuga a la plancha y ensalada': 6.8,
-        'Nuggets with potatoes': 6.8,
+        'Pechuga de pollo rebozado con patata': {
+            price: 5.5,
+            image: 'https://drive.google.com/uc?id=172OpGri9kjB6RP7wagAyUstC9Gw3pJLS'
+        },
+        'Croquetas con patatas fritas y huevos': {
+            price: 4,
+            image: 'https://drive.google.com/uc?id=1qq5mVrUWHfAoEAIC7C3NfPoKJFIRPg2V'
+        },
+        'Calamares a la romana con patata': {
+            price: 6.8,
+            image: 'https://drive.google.com/uc?id=1XXlRQ0D2yb05SEyKoTY8vnvIjE4qjuKO'
+        },
+        'Lomo con huevo y patatas': {
+            price: 5,
+            image: 'https://drive.google.com/uc?id=1pGm6-Dg3tRYh1lUIGvYv_tRT2h73GZA2'
+        },
+        'Salchicas con huevo y patatas fritas': {
+            price: 4.5,
+            image: 'https://drive.google.com/uc?id=1cBX7d0wyx7CunPw8cjxPWbtKif38-n-D'
+        },
+        'Frankfurt con patata': {
+            price: 4,
+            image: 'https://drive.google.com/uc?id=1HHS7nwZbQiZECfgP9t44EeSFB1xod3Ie'
+        },
+        'Butifarra con judía, huevo y pimiento verde': {
+            price: 5.5,
+            image: 'https://drive.google.com/uc?id=1aPT7_q_73woIaNTFTmpoLF6PKzXHh7mT'
+        },
+        'Bistec con patatas': {
+            price: 6.8,
+            image: 'https://drive.google.com/uc?id=1aU9iwn1wp5mqNpGwVz4yVTQhwpbU-3Lp'
+        },
+        'Espárrago a la plancha con jamón y tortilla a la francesa': {
+            price:  5.5,
+            image: ''
+        },/**/
+        'Beicon, chistorra, huevo y patatas': {
+            price: 5.5,
+            image: 'https://drive.google.com/uc?id=1SRJrDGdT0CM8LvRXV-oFHU2r45AEGeM'
+        } ,
+        'Chuletas de cerdo a la plancha':  {
+            price: 5.5,
+            image: 'https://drive.google.com/uc?id=1ahjN9rUXRdPwtMoPo8Elc_lkYjsS5wka'
+        },
+        'Chuletas de corderos a la plancha': {
+            price: 6.5,
+            image: 'https://drive.google.com/uc?id=1Itf5Oc1MyoHr2n2jlU7EzwgregxpKGQj'
+        },
+        'Merluza rebozada con patatas': {
+            price: 5.5,
+            image: 'https://drive.google.com/uc?id=1GvqsrelqB8RkTiHqjbm0Zz_x5r6PxDkg'
+        } ,
+        'Merluza a la plancha con patatas': {
+            price: 5.5,
+            image: 'https://drive.google.com/uc?id=1nNpFWdJXZVM2nJMhlOnUa6DYL0Wp_VIi'
+        } ,
+        'Lenguado a la plancha con patatas': {
+            price: 6.8,
+            image: 'https://drive.google.com/uc?id=1ju3JjQcO2eCw7NUQElKfHboAmxb0rEKq'
+        } ,
+        'Sepia a la plancha con patatas': {
+            price: 8,
+            image: 'https://drive.google.com/uc?id=1kBSWUq0lgE4zeXNh4oZgEgfY9yeoCCv4'
+        } ,
+        'Hamburguesa con huevos, ensalada y patatas': {
+            price: 5.8,
+            image: 'https://drive.google.com/uc?id=18oTN24A8RoHXyezwm-E2BxjKE4cZ7vjz'
+        } ,
+        'Chistorra, jamon, huevos y patatas': {
+            price: 6.5,
+            image: 'https://drive.google.com/uc?id=12nMNcK686_VsGaxAVbxRPBq9U8YbiTiG'
+        } ,
+        'Combinados fritos':  {
+            price: 6.8,
+            image: 'https://drive.google.com/uc?id=154otBHCgN6Yi-4eADdLes2svI2NZZjLz'
+        },
+        'Hamburguesa con patatas': {
+            price: 5,
+            image: 'https://drive.google.com/uc?id=1TXAc8utCGvpbHUyuCBYjtmR9smDMAq-9'
+        } ,
+        'Lenguado frito con patatas': {
+            price: 6.8,
+            image: 'https://drive.google.com/uc?id=1ju3JjQcO2eCw7NUQElKfHboAmxb0rEKq'
+        } ,
+        'Pincho de cerdo con patatas':  {
+            price: 4.8,
+            image: 'https://drive.google.com/uc?id=1nhz81sF7ElRRetqT7zyIKH30rFWglKBJ'
+        },
+        'Pechuga a la plancha y ensalada': {
+            price: 6.8,
+            image: 'https://drive.google.com/uc?id=18XmLQeJJCgKKPDbxS_JNu_x1WcQLaiGo'
+        } ,
+        'Nuggets with potatoes': {
+            price: 6.8,
+            image: 'https://drive.google.com/uc?id=1k0KJyiWJFRoc64mO0phE6sIhj3KMLz9B'
+        } ,
     }
 }
